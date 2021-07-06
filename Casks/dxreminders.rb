c766f5386823dd82b64399c7ed237ff1d1c3bd8d
David Vachulka <david@konstrukce-cad.com>
2021-07-06 15:06:12 +0200

    Update dxreminders.rb

--- Casks/dxreminders.rb
+++ Casks/dxreminders.rb
@@ -1,6 +1,6 @@
 cask 'dxreminders' do
-  version '1.10.1'
-  sha256 '68a0a7380999c8ab54156575688bfee1f49a55068f251e4611788305209c1465'
+  version '1.20.0'
+  sha256 'ce793c582844aacdeeb866003353617c5f3670bc34e6d44f556e630c1cad8e5c'
 
   url "https://files.dxsolutions.org/dxreminders-#{version}.dmg"
   name 'dxreminders'

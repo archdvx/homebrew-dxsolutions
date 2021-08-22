48b8f6ff73e572408851a6db1546146df77ede5e
David Vachulka <david@konstrukce-cad.com>
2021-08-22 15:45:44 +0200

    wxdfast fixed

--- Casks/wxdfast.rb
+++ Casks/wxdfast.rb
@@ -1,6 +1,6 @@
 cask 'wxdfast' do
   version '0.70.0'
-  sha256 'c2898f3eaed981f0b3daa5d026845970ac9a49d8236bd3c55e3f6c2622191021'
+  sha256 '2c1572bbd2b703fc761a9b627532409f279297949b9af1ce369f3c3b04ea1682'
 
   url "https://github.com/archdvx/wxdfast/releases/download/v#{version}/wxDownloadFast-#{version}.dmg"
   name 'wxDownload Fast'

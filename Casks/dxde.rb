daf303bea5e6c39433fc487a61536f5264913fd7
David Vachulka <david@konstrukce-cad.com>
2022-01-02 20:13:37 +0100

    Update dxde.rb
    Upgrade to new version 1.40.7

--- Casks/dxde.rb
+++ Casks/dxde.rb
@@ -1,13 +1,13 @@
 cask 'dxde' do
-  version '1.40.6'
-  sha256 'b1cfd57e5f5c8e665dda72df7945a81d8ac73272c831124d4af0dcf04df956df'
+  version '1.40.7'
+  sha256 'ab546c78b8bbe253ba3825719b6f3c463f2a27f9fc69be43bce1fed5ef01d634'
 
   url "https://files.dxsolutions.org/dxde-#{version}.dmg"
   name 'dxde'
   desc 'Program pro vedeni danove evidence (for czech tax records)'
   homepage 'https://dxde.dxsolutions.org/'
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :catalina"
 
   app "dxde.app"
 end

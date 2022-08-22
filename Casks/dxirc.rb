ac155fbfe60f48728130b59b1ede18645a00f375
David Vachulka <david@konstrukce-cad.com>
2022-08-23 00:23:10 +0200

    Update dxirc.rb

--- Casks/dxirc.rb
+++ Casks/dxirc.rb
@@ -1,6 +1,6 @@
 cask 'dxirc' do
-  version '1.40.0'
-  sha256 '631d25665229fe8b3b2baed14f6a4c2ac2eb6e2bd446a2a074bde4069ba8bf51'
+  version '1.40.1'
+  sha256 '1efec3e89c584cbf47b7ac5241cf9999f151251c2d92830f26cbce18950d7427'
 
   url "https://downloads.sourceforge.net/dxirc/dxirc-#{version}.dmg",
       verified: "downloads.sourceforge.net/dxirc/"
@@ -8,7 +8,7 @@ cask 'dxirc' do
   desc 'A simple IRC client'
   homepage 'https://dxirc.org/'
 
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :catalina"
 
   app "dxirc.app"
 end

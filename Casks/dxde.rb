f97a342321fb06ef3cde981b1fe7ae0ef195127a
David Vachulka <david@konstrukce-cad.com>
2023-04-10 18:22:52 +0200

    Update to dxde version 1.40.11

--- Casks/dxde.rb
+++ Casks/dxde.rb
@@ -1,6 +1,6 @@
 cask 'dxde' do
-  version '1.40.10'
-  sha256 'a90d2c307dade6a1a019ae34ffa64d5be5de42ccfad40215458764202793a0fa'
+  version '1.40.11'
+  sha256 '6bf916f970e6dded6c7bca00461abcb033766d67103877a4bba017c199800ff5'
 
   url "https://files.dxsolutions.org/dxde-#{version}.dmg"
   name 'dxde'

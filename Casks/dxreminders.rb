c539400f658af0199464094814ce92ca6b413be0
David Vachulka <david@konstrukce-cad.com>
2023-05-14 18:51:58 +0200

    Update dxreminders.rb
    New version 1.21.2

--- Casks/dxreminders.rb
+++ Casks/dxreminders.rb
@@ -1,13 +1,13 @@
 cask 'dxreminders' do
-  version '1.21.1'
-  sha256 'fcc1db46ce7045fd38547e3d431378475e6b4a35d974556753fba072f21a1add'
+  version '1.21.2'
+  sha256 '5299f2b50ae6c1b30ded53da583e8939b5bc22aac1635b6a773626ce51c9f464'
 
   url "https://files.dxsolutions.org/dxreminders-#{version}.dmg"
   name 'dxreminders'
   desc 'A simple program for reminders'
   homepage 'https://dxreminders.dxsolutions.org/'
 
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :bigsur"
 
   app "dxreminders.app"
 end

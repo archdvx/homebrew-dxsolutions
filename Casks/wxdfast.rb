7dcff1827ae278294cc6ae697ff393614370c401
David Vachulka <archdvx@dxsolutions.org>
2025-02-09 19:08:57 +0100

    Update wxdfast.rb

--- Casks/wxdfast.rb
+++ Casks/wxdfast.rb
@@ -1,6 +1,6 @@
 cask 'wxdfast' do
-  version '0.70.2'
-  sha256 '88d0cabf3dbd7ba96310804650aae73a88419b136baaad21fbcf7330abe3e479'
+  version '0.70.4'
+  sha256 '688e28403a1d50a9e7e9a703d5232f2c9b151f3e9edbe994004bbae9966c60ed'
 
   url "https://github.com/archdvx/wxdfast/releases/download/v#{version}/wxDownloadFast-#{version}.dmg"
   name 'wxDownload Fast'
